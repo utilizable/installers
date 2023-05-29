307db4227ad50f22af3ec967f20139a9b7cd1eb0
root <root@nvim.mshome.net>
2023-05-29 23:52:16 +0000

    comments, delete terraform tmp files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
+################################################################################
+## Script Name: main.yaml 
+## Description: 
+## Author: Igor Sadza 
+## Created: 2023-05-29 
+## Last Modified: 2023-05-29 
+## Version: 1.0
+#################################################################################
+
+
 variable "ssh_host" { type = string }
 variable "ssh_port" { type = string }
 variable "ssh_user" { type = string }
